64cdaf8522e2f36774d42fadd89c1f724038de36
Gavin <72846934+Gav06@users.noreply.github.com>
2021-07-30 13:34:44 -0400

    half broken ac

--- src/main/java/me/gavin/notorious/hack/hacks/combat/AutoCrystal.java
+++ src/main/java/me/gavin/notorious/hack/hacks/combat/AutoCrystal.java
@@ -1,7 +1,9 @@
 package me.gavin.notorious.hack.hacks.combat;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
+import com.sun.istack.internal.NotNull;
 import me.gavin.notorious.event.events.PacketEvent;
+import me.gavin.notorious.event.events.PlayerLivingUpdateEvent;
 import me.gavin.notorious.hack.Hack;
 import me.gavin.notorious.hack.RegisterHack;
 import me.gavin.notorious.hack.RegisterSetting;
@@ -10,10 +12,7 @@ import me.gavin.notorious.setting.BooleanSetting;
 import me.gavin.notorious.setting.ColorSetting;
 import me.gavin.notorious.setting.ModeSetting;
 import me.gavin.notorious.setting.NumSetting;
-import me.gavin.notorious.util.InventoryUtil;
-import me.gavin.notorious.util.NColor;
-import me.gavin.notorious.util.RenderUtil;
-import me.gavin.notorious.util.TickTimer;
+import me.gavin.notorious.util.*;
 import net.minecraft.block.Block;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
@@ -52,6 +51,9 @@ import java.util.stream.Collectors;
 public class AutoCrystal extends Hack {
 
     @RegisterSetting
+    private final NumSetting range = new NumSetting("Range", 4.5f, 1f, 6f, 0.5f);
+
+    @RegisterSetting
     private NumSetting attackDistance = new NumSetting("AttackRange", 4f, 1f, 6f, 1f);
     @RegisterSetting
     private NumSetting placeDistance = new NumSetting("PlaceRange", 4f, 1f, 6f, 1f);
@@ -78,231 +80,89 @@ public class AutoCrystal extends Hack {
     @RegisterSetting
     private BooleanSetting fastPlace = new BooleanSetting("FastPlace",true);
 
-    @Override
-    public String getMetaData() {
-        return " [" + ChatFormatting.GRAY + targetName + ChatFormatting.RESET + "]";
-    }
+    private EntityPlayer targetPlayer = null;
 
-    private final TickTimer breakTickTimer = new TickTimer();
-    private final TickTimer placeTickTimer = new TickTimer();
-
-    private ArrayList<BlockPos> placedCrystals = new ArrayList<>();
-
-    private EntityEnderCrystal targetCrystal;
-    private BlockPos lastPlacedPos;
-    private boolean outline = false;
-    private boolean fill = false;
-    private BlockPos targetedBlock = null;
-    private String targetName = "";
-    private int oldSlot;
-
-    @Override
-    public void onEnable() {
-        oldSlot = mc.player.inventory.currentItem;
-    }
-
-    @Override
-    public void onDisable() {
-        if(switchMode.getMode().equals("Normal")) {
-            InventoryUtil.switchToSlot(oldSlot);
-        }
-    }
-
+    /* :flushed: */
     @SubscribeEvent
-    public void onTick(TickEvent event) {
-        int oldSlot = mc.player.inventory.currentItem;
-        if (fastPlace.getValue()) {
-            if (mc.player.getHeldItemMainhand().getItem() == Items.END_CRYSTAL) {
-                ((IMinecraftMixin)mc).setRightClickDelayTimerAccessor(0);
-            }
-        }
-
-        int slot = InventoryUtil.getItemSlot(Items.END_CRYSTAL);
-        EnumHand hand = null;
-        if (switchMode.getMode().equals("Silent")) {
-            if (slot != -1) {
-                if (mc.player.isHandActive()) {
-                    hand = mc.player.getActiveHand();
-                }
-                mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
-            }
-        }else if (switchMode.getMode().equals("Normal")) {
-            InventoryUtil.switchToSlot(slot);
-        }
-
-        if(logic.getMode().equals("PlaceBreak")) {
-            doPlaceLogic();
-            doBreakLogic();
-        }else {
-            doBreakLogic();
-            doPlaceLogic();
-        }
-        targetedBlock = null;
-    }
-
-    @SubscribeEvent
-    public void onUpdate(RenderWorldLastEvent event) {
-        if(renderMode.getMode().equals("Both")) {
-            outline = true;
-            fill = true;
-        }else if(renderMode.getMode().equals("Outline")) {
-            outline = true;
-            fill = false;
-        }else {
-            fill = true;
-            outline = false;
-        }
-        if(targetedBlock != null) {
-            final AxisAlignedBB bb = new AxisAlignedBB(targetedBlock);
-            if(fill)
-                RenderUtil.renderFilledBB(bb, boxColor.getAsColor());
-            if(outline)
-                RenderUtil.renderOutlineBB(bb, outlineColor.getAsColor());
-        }
-    }
-
-    @SubscribeEvent
-    public void onPacket(PacketEvent.Receive event) {
-        if (!setDead.getValue())
-            return;
-
-        if (event.getPacket() instanceof SPacketSoundEffect) {
-            SPacketSoundEffect sound = (SPacketSoundEffect) event.getPacket();
-            if (sound.getSound() == SoundEvents.ENTITY_GENERIC_EXPLODE) {
-                if (canAttackCrystal(targetCrystal))
-                    targetCrystal.setDead();
-            }
-        }
-    }
-
-    private void doBreakLogic() {
-        targetCrystal = mc.world.loadedEntityList.stream()
-                .filter(e -> e.getDistance(mc.player) <= attackDistance.getValue())
-                .filter(e -> e instanceof EntityEnderCrystal)
-                .map(e -> (EntityEnderCrystal) e)
-                .filter(this::canAttackCrystal)
-                .findFirst().orElse(null);
-
-        if (targetCrystal == null)
-            return;
-
-        if (canAttackCrystal(targetCrystal) && breakTickTimer.hasTicksPassed((long) breakDelay.getValue())) {
-            mc.player.swingArm(EnumHand.MAIN_HAND);
-            mc.player.connection.sendPacket(new CPacketUseEntity(targetCrystal));
-            breakTickTimer.reset();
-        }
-    }
-
-    EntityPlayer targetPlayer;
-
-    private void doPlaceLogic() {
-
-        // finding target based on highest damage
-        List<BlockPos> crystalBlocks =
-                getBlocksAroundPlayer(placeDistance.getValue())
-                        .stream()
-                        .filter(this::canPlaceCrystal)
-                        .sorted(Comparator.comparing(blockPos -> mc.player.getDistanceSq(blockPos)))
-                        .collect(Collectors.toList());
-
-        BlockPos crystalPosition = null;
-
-        for (EntityPlayer e : mc.world.playerEntities) {
-            if (e.getDistance(mc.player) <= placeDistance.getValue()) {
-                if (e.equals(mc.player))
-                    continue;
-
-                targetPlayer = e;
-
-                targetName = e.getDisplayNameString();
-
-                double bestDamage = Double.MIN_VALUE;
-                BlockPos bestPos = null;
-
-                for (BlockPos pos : crystalBlocks) {
-                    // adding 0.5 to x for crystal width
-                    // adding 1.0 to y because crystal will be placed above
-                    double targetDamage = calculateDamage(pos.getX() + 0.5D, pos.getY() + 1, pos.getZ() + 0.5D, targetPlayer);
-                    double selfDmg = calculateDamage(pos.getX() + 0.5D, pos.getY() + 1, pos.getZ() + 0.5D, mc.player);
-
-                    if (targetDamage >= minDmg.getValue() && selfDmg <= maxSelfDmg.getValue()) {
-                        if (targetDamage > bestDamage) {
-                            bestDamage = targetDamage;
-                            bestPos = pos;
-                            targetedBlock = pos;
-                        }
-                    }
-                }
-
-                if (bestPos != null) {
-                    crystalPosition = bestPos;
-                }
-            }
-        }
-        if (crystalPosition != null) {
-            assert crystalPosition != null;
-            if (placeTickTimer.hasTicksPassed((long) placeDelay.getValue())) {
-                mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(crystalPosition, EnumFacing.UP, EnumHand.MAIN_HAND, 0f, 0f, 0f));
-                mc.player.swingArm(EnumHand.MAIN_HAND);
-                placeTickTimer.reset();
-                lastPlacedPos = crystalPosition;
-            }
-        }
-    }
-
-    private boolean canPlaceCrystal(BlockPos pos) {
-        // checking if blocks above are air
-        if (getBlock(pos.add(0, 1, 0)) == Blocks.AIR && getBlock(pos.add(0, 2, 0)) == Blocks.AIR) {
-
-            // seeing if it is obsidian or bedrock
-            if (getBlock(pos) == Blocks.OBSIDIAN || getBlock(pos) == Blocks.BEDROCK) {
-                // checking if nobody is standing directly above the block
-                BlockPos air1 = pos.add(0, 1, 0);
-                BlockPos air2 = pos.add(0, 2, 0);
-
-                return mc.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(air1)).isEmpty()
-                        && mc.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(air2)).isEmpty();
-
-            } else {
-                return false;
-            }
+    public void onEjaculate(PlayerLivingUpdateEvent event) {
+        if (logic.getMode().equals("PlaceBreak")) {
+            place();
+            break_();
         } else {
-            return false;
+            break_();
+            place();
         }
     }
 
-    private Block getBlock(BlockPos pos) {
+    private void place() {
+        if (mc.player.getHeldItemMainhand().getItem() != Items.END_CRYSTAL)
+            return;
+
+        if (targetPlayer == null) {
+            targetPlayer = findPlayerTarget();
+        } else {
+            if (!isTargetStillViable(targetPlayer)) {
+                targetPlayer = null;
+                return;
+            }
+
+            final BlockPos pos = getBestPlacementPosition(targetPlayer);
+            if (pos != null) {
+                mc.playerController.processRightClickBlock(mc.player, mc.world, pos, EnumFacing.UP, Vec3d.ZERO, EnumHand.MAIN_HAND);
+                mc.player.swingArm(EnumHand.MAIN_HAND);
+            }
+        }
+    }
+
+    private BlockPos getBestPlacementPosition(@NotNull EntityPlayer player) {
+        double bestDamage = -1;
+        BlockPos bestPosition = null;
+        for (BlockPos pos : BlockUtil.getSurroundingBlocks((int)placeDistance.getValue(), true)) {
+            if (canPlaceCrystal(pos)) {
+                double damage = calculateDamage(pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, player);
+                if (damage > bestDamage) {
+                    bestDamage = damage;
+                    bestPosition = pos;
+                }
+            }
+        }
+
+        return bestPosition;
+    }
+
+    private boolean canPlaceCrystal(@NotNull BlockPos pos) {
+        return getBlock(pos) == Blocks.OBSIDIAN
+                && getBlock(pos.add(0, 1, 0)) == Blocks.AIR
+                && getBlock(pos.add(0, 2, 0)) == Blocks.AIR
+                && mc.world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(pos.add(0, 1, 0))).size() == 0;
+    }
+
+    private Block getBlock(@NotNull BlockPos pos) {
         return mc.world.getBlockState(pos).getBlock();
     }
 
-    private boolean canAttackCrystal(EntityEnderCrystal targetCrystal) {
-        if (targetCrystal != null) {
-            if (mc.player.getDistance(targetCrystal) <= attackDistance.getValue()) {
-                return !targetCrystal.isDead;
-            }
-        }
+    private void break_() {
 
-        return false;
     }
 
-    private ArrayList<BlockPos> getBlocksAroundPlayer(float range) {
-        ArrayList<BlockPos> posList = new ArrayList<>();
-        for (float x = -range; x < range; x++) {
-            // loop for y axis
-            for (float y = range + 1; y > -range; y--) {
-                // loop for z axis
-                for (float z = -range; z < range; z++) {
-                    BlockPos pos = new BlockPos(x, y, z);
-                    posList.add(pos.add(mc.player.getPosition()));
-                }
-            }
-        }
-
-        return posList;
+    private EntityPlayer findPlayerTarget() {
+        return mc.world.playerEntities.stream()
+                .filter(player -> !player.equals(mc.player))
+                .filter(player -> player.getDistance(mc.player) <= range.getValue())
+                .filter(player -> player.getHealth() > 0)
+                .filter(EntityLivingBase::isEntityAlive)
+                .filter(player -> !player.isDead)
+                .findFirst().orElse(null);
     }
 
-    // credit to srgantmoomoo and postman for these methods
-    // i was too lazy to write these 3 by myself
+    private boolean isTargetStillViable(EntityPlayer player) {
+        return  player.getDistance(mc.player) <= range.getValue() && player.getHealth() > 0 && player.isEntityAlive() && !player.isDead;
+    }
+
+//    private EntityEnderCrystal findCrystalTarget() {
+//
+//    }
+
     public float getBlastReduction(EntityLivingBase entity, float damage, Explosion explosion) {
         if (entity instanceof EntityPlayer) {
             EntityPlayer ep = (EntityPlayer) entity;
