44649349d9ac811807fde62e0b5141fe3490f246
Gavin <72846934+Gav06@users.noreply.github.com>
2021-08-14 00:40:20 -0400

    big black nigga balls (HD)

--- src/main/java/me/gavin/notorious/manager/ConfigManager.java
+++ src/main/java/me/gavin/notorious/manager/ConfigManager.java
@@ -1,32 +1,44 @@
 package me.gavin.notorious.manager;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import com.google.gson.stream.JsonWriter;
 import me.gavin.notorious.Notorious;
 import me.gavin.notorious.NotoriousMod;
+import me.gavin.notorious.friend.Friend;
 import me.gavin.notorious.hack.Hack;
 import me.gavin.notorious.setting.*;
 import net.minecraft.client.Minecraft;
 
 import java.awt.*;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Iterator;
 
 public class ConfigManager {
 
+    private final JsonParser parser;
     private final Gson gson;
     private final File saveDir;
+    private final File hackDir;
+    private final File otherDir;
 
     public ConfigManager() {
+        parser = new JsonParser();
         gson = new GsonBuilder().setPrettyPrinting().create();
-        saveDir = new File(Minecraft.getMinecraft().gameDir, NotoriousMod.MOD_ID);
-        if (!saveDir.exists())
-            saveDir.mkdir();
+        saveDir = makeDir(new File(Minecraft.getMinecraft().gameDir, NotoriousMod.MOD_ID));
+        otherDir = makeDir(new File(saveDir, "other"));
+        hackDir = makeDir(new File(saveDir, "hacks"));
+    }
+
+    private File makeDir(File file) {
+        if (!file.exists())
+            file.mkdir();
+        return file;
+    }
+
+    private File makeFile(File file) throws IOException {
+        if (!file.exists())
+            file.createNewFile();
+        return file;
     }
 
     public void save() throws IOException {
@@ -48,23 +60,26 @@ public class ConfigManager {
                 }
             }
 
-            final File saveFile = new File(saveDir, hack.getName() +".json");
-            if (!saveFile.exists())
-                saveFile.createNewFile();
+            final File saveFile = makeFile(new File(hackDir, hack.getName() +".json"));
             final FileWriter writer = new FileWriter(saveFile);
-            gson.toJson(hackObj, writer);
-            writer.flush();
-            writer.close();
+            write(hackObj, writer);
         }
+
+        final JsonArray friendList = new JsonArray();
+        for (Friend friend : Notorious.INSTANCE.friend.getFriends()) {
+            final JsonObject obj = new JsonObject();
+            obj.addProperty("name", friend.getName());
+            friendList.add(obj);
+        }
+        write(friendList, new FileWriter(makeFile(new File(otherDir, "friends.json"))));
     }
 
     public void load() throws IOException {
         for (Hack hack : Notorious.INSTANCE.hackManager.getHacks()) {
-            final File file = new File(saveDir, hack.getName() + ".json");
+            final File file = new File(hackDir, hack.getName() + ".json");
             if (!file.exists())
                 continue;
 
-            final JsonParser parser = new JsonParser();
             final FileReader reader = new FileReader(file);
             final JsonObject object = (JsonObject) parser.parse(reader);
             reader.close();
@@ -97,5 +112,30 @@ public class ConfigManager {
                 }
             }
         }
+
+        // friend
+        final File friendFile = new File(otherDir, "friends.json");
+        if (friendFile.exists()) {
+            final JsonArray parsedArray = (JsonArray) parse(new FileReader(friendFile));
+
+            final Iterator<JsonElement> it = parsedArray.iterator();
+
+            while (it.hasNext()) {
+                final JsonObject obj = (JsonObject) it.next();
+                Notorious.INSTANCE.friend.addFriend(obj.get("name").getAsString());
+            }
+        }
+    }
+
+    private void write(JsonElement element, Writer writer) throws IOException {
+        gson.toJson(element, writer);
+        writer.flush();
+        writer.close();
+    }
+
+    private JsonElement parse(Reader reader) throws IOException {
+        final JsonElement ele = parser.parse(reader);
+        reader.close();
+        return ele;
     }
 }
