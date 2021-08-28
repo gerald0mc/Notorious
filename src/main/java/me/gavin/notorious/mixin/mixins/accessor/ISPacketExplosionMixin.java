1086bb862cff7f8ceef100d25e34fbcb7ea055fc
zihasz <ihaszzeno@gmail.com>
2021-08-28 17:13:16 +0200

    rename a mixin

--- src/main/java/me/gavin/notorious/mixin/mixins/accessor/ISPacketExplosionMixin.java
+++ src/main/java/me/gavin/notorious/mixin/mixins/accessor/ISPacketExplosionMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(SPacketExplosion.class)
-public interface ISpacketExplosionMixin {
+public interface ISPacketExplosionMixin {
 
     @Accessor("motionX")
     void setMotionXAccessor(float value);
